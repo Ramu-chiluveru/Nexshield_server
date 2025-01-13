4302701f10231ae236de5a95d42097ef52261b4c
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-01-13 12:13:07 +0530

    add keyword funnality fixed bugs

--- routes/userRoutes.cjs
+++ routes/userRoutes.cjs
@@ -1,9 +1,10 @@
 const express = require("express");
 const Router = express.Router();
-const { loginController, registerController, fetchAllUsersController } = require("../controllers/userController.cjs");
-const protect = require("../middleWare/authMiddleware.cjs");
+const { loginController, registerController, fetchAllUsersController, addKeywordsController, editUserController, getKeywordsController } = require("../controllers/userController.cjs");
 
-Router.post('/login',loginController);
-Router.post('/register',registerController);
-
+Router.post('/login', loginController);
+Router.post('/register', registerController);
+Router.post('/addkeywords', addKeywordsController);
+Router.post('/edituser', editUserController);
+Router.get('/getkeywords/:userId', getKeywordsController); 
 module.exports = Router;
